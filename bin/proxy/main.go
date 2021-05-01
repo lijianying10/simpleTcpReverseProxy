7b8a003404e33ae3f21cedbe3b5a44b4c4cec285
jianying.li <lijianying12@gmail.com>
2021-05-01 15:18:39 +0000

    update

--- bin/proxy/main.go
+++ bin/proxy/main.go
@@ -7,9 +7,7 @@ import (
 )
 
 var serviceListenAddr *string = flag.String("l", "127.0.0.1:8080", "service listen addr")
-
-//var configFilePath *string = flag.String("c", "/etc/simpleTcpReverseProxy.json", "config file path")
-var configFilePath *string = flag.String("c", "/tmp/simpleTcpReverseProxy.json", "config file path")
+var configFilePath *string = flag.String("c", "/etc/simpleTcpReverseProxy.json", "config file path")
 
 func main() {
 	flag.Parse()

dbfbf31ce58bf477e79614acdc70a2552656332b
jianying.li <lijianying12@gmail.com>
2021-05-02 09:36:12 +0000

    add systemd notify support

--- bin/proxy/main.go
+++ bin/proxy/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"time"
 
+	"github.com/coreos/go-systemd/daemon"
 	"github.com/lijianying10/simpleTcpReverseProxy/pkg/service"
 )
 
@@ -10,7 +13,22 @@ var serviceListenAddr *string = flag.String("l", "127.0.0.1:8080", "service list
 var configFilePath *string = flag.String("c", "/etc/simpleTcpReverseProxy.json", "config file path")
 
 func main() {
+	fmt.Println("server starting....")
 	flag.Parse()
+	fmt.Println("Listen on :", *serviceListenAddr)
 	rt := service.NewRuntime(*serviceListenAddr, *configFilePath)
+	daemon.SdNotify(false, daemon.SdNotifyReady)
+	go keepAlive()
 	rt.Run()
+}
+
+func keepAlive() {
+	interval, err := daemon.SdWatchdogEnabled(false)
+	if err != nil || interval == 0 {
+		return
+	}
+	for {
+		daemon.SdNotify(false, daemon.SdNotifyWatchdog)
+		time.Sleep(interval / 3)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/lijianying10/simpleTcpReverseProxy
 
 go 1.16
 
-require github.com/julienschmidt/httprouter v1.3.0
+require (
+	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf
+	github.com/julienschmidt/httprouter v1.3.0
+)
